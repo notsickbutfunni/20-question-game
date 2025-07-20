9a9d45d7caa45ae3b648ed975508b28cb04c9cfd
Inkar Khairatkyzy <ink000125@gmail.com>
2025-07-20 13:26:34 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# 20-question-game
+# 20 Questions Game (Decision Tree + Flask)
+
+Web-app in "20 Questions" style using Decision tree algorithm for predicting anime characters from answers yes/no
+
+
+## Project structure
+```
+20q-game/
+├── app.py 
+├── model.ipynb #just training and testing
+├── model.pkl 
+├──dataset/
+    └── data.csv #trained data
+    └── anime_traits_better.csv #original data from kaggle
+    └── output.png #visualization of trained model
+└── templates/
+    └── index.html 
+```
+
+
--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
 import joblib
+import pandas as pd
+import json
+from sklearn.tree import DecisionTreeClassifier, export_text
 
 app = Flask(__name__)
 
-model = joblib.load('model.pkl')  
+model = joblib.load('model.pkl') 
+df = pd.read_csv(r"C:\Users\kaira\DTree\dataset\data.csv")
+X = df.drop(columns=['Names'], axis=1)
+y = df['Names']
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return render_template('index.html')
+clf = DecisionTreeClassifier(max_depth=None)
+clf.fit(X, y)
 
-@app.route('/', methods=['POST'])
-def predict():
-    return "POST request received"
+feature_names = list(X.columns)
+tree_text = export_text(model, feature_names=feature_names)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=3000, debug =True)
